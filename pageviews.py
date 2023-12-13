eaa768054368e34f41c6af1758242422b390d50a
ahask93 <ahask93@gmail.com>
2023-12-12 19:57:55 -0500

    submission functions fully complete!

--- pageviews.py
+++ pageviews.py
@@ -21,12 +21,6 @@ def db_submit_donation(donor_name, monetary_donation, toy_donation): #code for s
 
 def donations():
     st.header("Make a Donation!")
-    if 'donor_name' not in st.session_state:
-        st.sesson_state['donor_name'] = ''
-    if 'monetary_donation' not in st.session_state:
-        st.session_state['monetary_donation'] = ''
-    if 'toy_donation' not in st.session_state:
-        st.session_state['toy_donation'] = ''
     donor_name = st.text_input("Enter your Full Name OR your organization's name")
     monetary_donation = st.text_input("Monetary Donation amount:")
     toy_donation = st.text_input("Toy being donated:")
@@ -42,14 +36,7 @@ def donations():
     if st.button("Submit Donation"):
         db_submit_donation(donor_name, monetary_donation, toy_donation)
         st.session_state.submitted = True
-        st.session_state.donor_name = ""
-        st.session_state.monetary_donation = ""
-        st.session_state.toy_donation = ""
-        st.session_state.submitted = False
         st.success("Donation submitted! Thank you for your generosity!")
-    #the "on_click" will be the code to create an instance of the Sponsors Class
-    # and send it's attributes to the DB, 
-    #create function and call in the on_Click
 
 def db_submit_request(parent_name, child_name, child_age, toy_requested): #code for sending request info to db
     collection_name = dbname["Requested"]
@@ -59,35 +46,21 @@ def db_submit_request(parent_name, child_name, child_age, toy_requested): #code 
         "child_age": child_age,
         "toy_requested": toy_requested
     })
-    st.session_state.submitted = False
+    st.session_state.submitted = True
 
 
 def makeReq():
     st.header("Request a Donation!")
-    if 'parent_name' not in st.session_state:
-        st.session_state['parent_name'] = ''
-    if 'child_name' not in st.session_state:
-        st.session_state['child_name'] = ''
-    if 'child_age' not in st.session_state:
-        st.session_state['child_age'] = ''
-    if 'toy_requested' not in st.session_state:
-        st.session_state['toy_requested'] = ''
-    parent_name = st.text_input("What is your first and last name?", value = st.session_state['parent_name'])
-    child_name = st.text_input("What is the child's first and last name?", value = st.session_state['child_name'])
-    child_age = st.text_input("How old is the child?", value = st.session_state['child_age'])
-    toy_requested = st.text_input("What toy are you requesting?", value = st.session_state['toy_requested'])
+
+    parent_name = st.text_input("What is your first and last name?")
+    child_name = st.text_input("What is the child's first and last name?")
+    child_age = st.text_input("How old is the child?")
+    toy_requested = st.text_input("What toy are you requesting?")
+    
     if st.button("Submit Donation Request"):
         db_submit_request(parent_name, child_name, child_age, toy_requested)
         st.session_state.submitted = True
-        st.session_state.parent_name = ""
-        st.session_state.child_name = ""
-        st.session_state.child_age = ""
-        st.session_state.toy_requested = ""
-        st.session_state.submitted = False
         st.success("Request submitted!")
-        #the "on_click" will be the code to send to DB, 
-        #create function and call here
-        #need to add code to button to have it send the entered info to the MongoDB database
         # ref https://docs.streamlit.io/library/api-reference/widgets/st.button
 
 @st.cache_data(ttl=600)
