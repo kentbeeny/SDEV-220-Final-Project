10853a0d719eff2d9bfbc4e2915727aea74c47ce
ahask93 <ahask93@gmail.com>
2023-12-14 18:53:35 -0500

    added code to remove need for ssl cert

--- getDb.py
+++ getDb.py
@@ -2,7 +2,7 @@ from pymongo import MongoClient
 
 def get_database():
     CONNECTION_STRING = "mongodb+srv://ahask93:finalproject@cluster1.itmbuuh.mongodb.net/"
-    client = MongoClient(CONNECTION_STRING)
+    client = MongoClient(CONNECTION_STRING, ssl_cert_reqs=ssl.CERT_NONE)
 
     return client['TFT']
 
