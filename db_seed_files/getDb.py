66acec1178059a9b7843043ea2227f07423272f0
ahask93 <ahask93@gmail.com>
2023-12-14 19:23:50 -0500

    added certifi to getDb in seedfile folder the right way this time part 2

--- db_seed_files/getDb.py
+++ db_seed_files/getDb.py
@@ -4,7 +4,7 @@ ca=certifi.where()
 
 def get_database():
     CONNECTION_STRING = "mongodb+srv://ahask93:finalproject@cluster1.itmbuuh.mongodb.net/"
-    client = MongoClient(CONNECTION_STRING)
+    client = MongoClient(CONNECTION_STRING, tlsCAFile=ca)
 
     return client['TFT']
 
